705fbf7aaaf565277228499d64fdfbcff672367d
Gideon <efiakedohogideon@gmail.com>
2025-10-23 07:12:13 +0100

    modified:   css/styles.css 	modified:   index.html 	modified:   js/main.js 	new file:   next.js 	new file:   src/App.jsx 	new file:   src/components/RSVPandGifts.jsx 	new file:   src/components/app.jsx 	new file:   src/data/weddingData.json

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import RSVPandGifts from "./components/RSVPandGifts";
+
+function App() {
+    return (
+        <div className="App">
+            {/* Other sections */}
+            <RSVPandGifts />
+            {/* Footer or additional sections */}
+        </div>
+    );
+}
+
+export default App;
